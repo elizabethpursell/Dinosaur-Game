6b69629c7b28dcb2703bb43a822122d86a3b7ca5
Elizabeth Pursell <emp520@lehigh.edu>
2024-05-06 13:58:04 -0400

    fixing initial commit

--- app.js
+++ app.js
@@ -15,19 +15,4 @@ app.use(express.static(
     path.resolve(__dirname, "public")
 ));
 
-app.listen(3000, () => console.log("Starting up Bouncing Rectangles"));
-
-var wordList = [];
-fsPromises.readFile("./words.txt")
-    .then(function (result) {
-        wordList = ("" + result).split("\n");
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-
-app.get("/word", (req, res) => {
-    let retObj = {"word": wordList[Math.floor(Math.random() * wordList.length)]};
-    const ret = JSON.stringify(retObj);
-    res.end(ret);
-});
+app.listen(3000, () => console.log("Starting up Dinosaur Game"));
